c047641eeecd461a75af0db557049e9da14f3da6
PARKJUNYONG <treeoark1@gmail.com>
2026-08-19 13:24:26 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/example/kwordpocket/KWordPocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.kwordpocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KWordPocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KWordPocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kwordpocket/KWordPocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kwordpocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KWordPocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
