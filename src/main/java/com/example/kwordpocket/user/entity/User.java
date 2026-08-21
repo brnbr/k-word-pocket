32e2fca9c1a8aa0e0b93ae8f9017217af17c11db
Inn <dulsfnj413@naver.com>
2026-08-21 10:33:54 +0900

    feat: Make UserEntity

--- src/main/java/com/example/kwordpocket/user/entity/User.java
+++ src/main/java/com/example/kwordpocket/user/entity/User.java
@@ -1,4 +1,44 @@
 package com.example.kwordpocket.user.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User(String email, String password, Role role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    private User(Long id) {
+        this.id = id;
+    }
+
+    public static User fromAuthUser(AuthUser authUser) {
+        return new User(authUser.getId());
+    }
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
 }
