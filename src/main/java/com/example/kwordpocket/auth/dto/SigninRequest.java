a791b2d23011adc343e5903ce6e51430a1addc1c
Inn <dulsfnj413@naver.com>
2026-08-21 17:42:53 +0900

    feat: make sign-in request

---
+++ src/main/java/com/example/kwordpocket/auth/dto/SigninRequest.java
@@ -0,0 +1,14 @@
+package com.example.kwordpocket.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class SigninRequest {
+
+    @Email
+    private String email;
+    @NotBlank
+    private String password;
+}
