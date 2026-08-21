77bef752a8dabe9838add13f445b298cf9d8c292
Inn <dulsfnj413@naver.com>
2026-08-21 11:23:46 +0900

    feat: make signupReqest

--- src/main/java/com/example/kwordpocket/user/dto/SignupRequest.java
+++ src/main/java/com/example/kwordpocket/user/dto/SignupRequest.java
@@ -1,4 +1,16 @@
 package com.example.kwordpocket.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
 public class SignupRequest {
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(max = 12, message = "비밀번호는 최대 12자까지 입력할 수 있습니다.")
+    private String password;
 }
