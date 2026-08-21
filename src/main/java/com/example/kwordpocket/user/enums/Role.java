55d8e811548b89816910c95717e383f79813dc16
Inn <dulsfnj413@naver.com>
2026-08-21 10:37:24 +0900

    feat: make RoleEnum

---
+++ src/main/java/com/example/kwordpocket/user/enums/Role.java
@@ -0,0 +1,13 @@
+package com.example.kwordpocket.user.enums;
+
+public enum Role {
+
+    ROLE_USER(Authority.USER),
+    ROLE_ADMIN(Authority.ADMIN);
+
+    private final String userRole;
+
+    public static Role of(String userRole) {
+        return Arrays.
+    }
+}
