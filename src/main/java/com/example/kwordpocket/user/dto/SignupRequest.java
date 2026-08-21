e046e08e0135bae1282de4da3099713884829090
Inn <dulsfnj413@naver.com>
2026-08-21 11:31:28 +0900

    feat: make signupRequest

--- src/main/java/com/example/kwordpocket/user/dto/SignupRequest.java
+++ src/main/java/com/example/kwordpocket/user/dto/SignupRequest.java
@@ -1,5 +1,6 @@
 package com.example.kwordpocket.user.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -8,6 +9,7 @@ import lombok.Getter;
 public class SignupRequest {
 
     @NotBlank
+    @Email
     private String email;
 
     @NotBlank
