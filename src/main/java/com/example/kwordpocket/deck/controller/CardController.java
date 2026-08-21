b0d1b81565195480c5e42e0d16f73e77ae5e0034
piepenie <piepenie@daum.net>
2026-08-21 12:15:27 +0900

    fix: ëAPI 엔endpoint fix & basic front add

--- src/main/java/com/example/kwordpocket/deck/controller/CardController.java
+++ src/main/java/com/example/kwordpocket/deck/controller/CardController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/cards")
+@RequestMapping("/cards")
 @RequiredArgsConstructor
 public class CardController {
 
