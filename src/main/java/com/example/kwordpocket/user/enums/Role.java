aaca8f144b875cd9a8bfbb105dc64a31e05cf8e0
Inn <dulsfnj413@naver.com>
2026-08-21 10:50:47 +0900

    refactor: move Role enum to enums package

--- src/main/java/com/example/kwordpocket/user/enums/Role.java
+++ src/main/java/com/example/kwordpocket/user/enums/Role.java
@@ -1,5 +1,10 @@
 package com.example.kwordpocket.user.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
 
     ROLE_USER(Authority.USER),
@@ -7,7 +12,17 @@ public enum Role {
 
     private final String userRole;
 
-    public static Role of(String userRole) {
-        return Arrays.
+    public static Role of(String role) {
+        return Arrays.stream(Role.values())
+                .filter(value -> value.name().equalsIgnoreCase(role))
+                .findFirst()
+                .orElseThrow(() -> new RoleNotMatchException());
     }
+
+    public static class Authority {
+        public static final String USER = "ROLE_USER";
+        public static final String ADMIN = "ROLE_ADMIN";
+    }
+
 }
+
