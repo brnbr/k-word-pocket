748ea80fb1e41a9b3a5f953edadbe621fc37cd78
Inn <dulsfnj413@naver.com>
2026-08-24 04:37:54 +0900

    feat: 유효성 검사 실패 예외 처리 추가

--- src/main/java/com/example/kwordpocket/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/kwordpocket/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.example.kwordpocket.common.exception;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -21,4 +22,13 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse("NOT_FOUND", e.getMessage()));
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        String defaultMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse("BAD_REQUEST", defaultMessage));
+    }
+
 }
